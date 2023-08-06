849c2433009b0a1b55d1f7a59f69910fdc2b3248
trinadh <trinadhbommineedu@gmail.com>
2023-08-06 08:40:50 +0530

    jfjfjf

---
+++ src/readmeaddcom/c/add.java
@@ -0,0 +1,21 @@
+package readmeaddcom.c;
+
+public class add {
+	
+	
+	public void m1() {
+		
+		System.out.println("hooooooooo");
+		
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		add a = new add();
+		a.m1();
+		
+	}
+
+}
